f4ada3fad089a63383da5fa8d393aa2926a71590
Christopher Bero <berocs@acedb.co>
2016-06-25 21:06:10 -0500

    Debugging and speed change. Added Red led debugging, usart debugging. Changed comm speed from 100kHz to 400kHz.

--- i2c.c
+++ i2c.c
@@ -3,7 +3,8 @@
  * Christopher Bero <berocs@acedb.co>
  */
 
-#include "i2c_master.h"
+#include "i2c.h"
+#include "usart.h"
 
 /**
  * TWSR Status Codes (m328p datasheet p218)
@@ -16,12 +17,40 @@
  * 0x38 - Arbitration lost in SLA+W or data transmit
  */
 
-void i2c_init(void)
+void i2c_init()
 {
 	// atmel Datasheet p232
-	TWSR = (0<<TWPS0) + (0<<TWPS1);
+	TWSR &= ~((1 << TWPS1) | (1 << TWPS0));
 	// atmel Datasheet p212
 	TWBR = (uint8_t)TWBR_val;
+}
+
+// http://www.avrfreaks.net/forum/twi-reset-when-communication-no-response
+void i2c_recover()
+{
+	printf("Recover i2c.\n");
+	uint8_t index;
+
+	TWSR = 0x00;
+	TWCR = 0x00;
+	DDRC |= (1 << PC5);
+	DDRC &= ~(1 << PC4);
+	PORTC &= ~(1 << PC4);
+	PORTC &= ~(1 << PC5);
+
+	for(index = 0; index < 9; index++)
+	{
+		PORTC |= (1 << PC5);
+		_delay_ms(1);
+		if (PINC & (1 << PC4)) // Look for SDA high
+		{
+			break;
+		}
+		PORTC &= ~(1 << PC5);
+	}
+
+	DDRC &= ~((1 << PC5) | (1 << PC4));
+	i2c_init();
 }
 
 uint8_t i2c_start(uint8_t address)
@@ -33,6 +62,7 @@ uint8_t i2c_start(uint8_t address)
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	// wait for end of transmission
 	timeout = 0;
+	OCR1A = 2;
 	while( !(TWCR & (1<<TWINT)) )
 	{
 		_delay_us(50);
@@ -40,6 +70,7 @@ uint8_t i2c_start(uint8_t address)
 		if (timeout > 20)
 			return 1;
 	}
+	OCR1A = 0;
 
 	// check if the start condition was successfully transmitted
 	if((TWSR & 0xF8) != TW_START){ return 1; }
@@ -50,6 +81,7 @@ uint8_t i2c_start(uint8_t address)
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	// wait for end of transmission
 	timeout = 0;
+	OCR1A = 2;
 	while( !(TWCR & (1<<TWINT)) )
 	{
 		_delay_us(50);
@@ -57,6 +89,7 @@ uint8_t i2c_start(uint8_t address)
 		if (timeout > 20)
 			return 1;
 	}
+	OCR1A = 0;
 
 	// check if the device has acknowledged the READ / WRITE mode
 	uint8_t twst = TW_STATUS & 0xF8;
@@ -67,46 +100,89 @@ uint8_t i2c_start(uint8_t address)
 
 uint8_t i2c_write(uint8_t data)
 {
+	uint8_t timeout;
 	// load data into data register
 	TWDR = data;
 	// start transmission of data
 	TWCR = (1<<TWINT) | (1<<TWEN);
+
 	// wait for end of transmission
-	while( !(TWCR & (1<<TWINT)) );
+	timeout = 0;
+	OCR1A = 2;
+	while( !(TWCR & (1<<TWINT)) )
+	{
+		_delay_us(50);
+		timeout++;
+		if (timeout > 20)
+			return 1;
+	}
+	OCR1A = 0;
 
 	if( (TWSR & 0xF8) != TW_MT_DATA_ACK ){ return 1; }
 
 	return 0;
 }
 
-uint8_t i2c_read_ack(void)
+uint8_t i2c_read_ack(uint8_t * data)
 {
+	uint8_t timeout;
 	// start TWI module and acknowledge data after reception
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWEA);
+
 	// wait for end of transmission
-	while( !(TWCR & (1<<TWINT)) );
+	timeout = 0;
+	OCR1A = 2;
+	while( !(TWCR & (1<<TWINT)) )
+	{
+		_delay_us(50);
+		timeout++;
+		if (timeout > 20)
+			return 1;
+	}
+	OCR1A = 0;
+
+	if (data)
+		*data = TWDR;
+
 	// return received data from TWDR
-	return TWDR;
+	return 0;
 }
 
-uint8_t i2c_read_nack(void)
+uint8_t i2c_read_nack(uint8_t * data)
 {
+	uint8_t timeout;
 	// start receiving without acknowledging reception
 	TWCR = (1<<TWINT) | (1<<TWEN);
+
 	// wait for end of transmission
-	while( !(TWCR & (1<<TWINT)) );
+	timeout = 0;
+	OCR1A = 2;
+	while( !(TWCR & (1<<TWINT)) )
+	{
+		_delay_us(50);
+		timeout++;
+		if (timeout > 20)
+			return 1;
+	}
+	OCR1A = 0;
+
+	if (data)
+		*data = TWDR;
+
 	// return received data from TWDR
-	return TWDR;
+	return 0;
 }
 
 uint8_t i2c_transmit(uint8_t address, uint8_t* data, uint16_t length)
 {
 	if (i2c_start(address | I2C_WRITE)) return 1;
 
+	OCR1A = 2;
 	for (uint16_t i = 0; i < length; i++)
 	{
 		if (i2c_write(data[i])) return 1;
 	}
+	OCR1A = 0;
 
 	i2c_stop();
 
@@ -117,11 +193,13 @@ uint8_t i2c_receive(uint8_t address, uint8_t* data, uint16_t length)
 {
 	if (i2c_start(address | I2C_READ)) return 1;
 
+	OCR1A = 2;
 	for (uint16_t i = 0; i < (length-1); i++)
 	{
-		data[i] = i2c_read_ack();
+		 if (i2c_read_ack(data+i)) return 1;
 	}
-	data[(length-1)] = i2c_read_nack();
+	if (i2c_read_nack(data+(length-1))) return 1;
+	OCR1A = 0;
 
 	i2c_stop();
 
@@ -134,10 +212,12 @@ uint8_t i2c_writeReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t l
 
 	i2c_write(regaddr);
 
+	OCR1A = 2;
 	for (uint16_t i = 0; i < length; i++)
 	{
 		if (i2c_write(data[i])) return 1;
 	}
+	OCR1A = 0;
 
 	i2c_stop();
 
@@ -152,11 +232,13 @@ uint8_t i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t le
 
 	if (i2c_start(devaddr | 0x01)) return 1;
 
+	OCR1A = 2;
 	for (uint16_t i = 0; i < (length-1); i++)
 	{
-		data[i] = i2c_read_ack();
+		if (i2c_read_ack(data+i)) return 1;
 	}
-	data[(length-1)] = i2c_read_nack();
+	if (i2c_read_nack(data+(length-1))) return 1;
+	OCR1A = 0;
 
 	i2c_stop();
 
@@ -165,17 +247,17 @@ uint8_t i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t le
 
 void i2c_stop(void)
 {
-	uint8_t timeout;
+//	uint8_t timeout;
 	// transmit STOP condition
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-	timeout = 0;
-	while( !(TWCR & (1<<TWINT)) )
-	{
-		_delay_us(50);
-		timeout++;
-		if (timeout > 20)
-		{
-			return 1;
-		}
-	}
+//	timeout = 0;
+//	while( !(TWCR & (1<<TWINT)) );
+//	{
+//		_delay_us(50);
+//		timeout++;
+//		if (timeout > 20)
+//		{
+//			return;
+//		}
+//	}
 }
--- i2c.h
+++ i2c.h
@@ -4,7 +4,7 @@
  */
 
 #ifndef I2C_MASTER_H
-#define I2C_MASTER_H
+#define I2C_MASTER_H 1
 
 #ifndef  F_CPU
 #	error i2c_master.h needs you to define F_CPU
@@ -15,18 +15,19 @@
 #include <stdint.h>
 #include <util/delay.h>
 
-#define F_SCL 100000UL // SCL frequency
+#define F_SCL 400000UL // SCL frequency
 #define Prescaler 1 // TWPS0 = TWPS1 = 0x0
 #define TWBR_val ( ( (F_CPU / F_SCL) - 16 ) / (Prescaler * 2) )
 
 #define I2C_READ 0x01
 #define I2C_WRITE 0x00
 
-void i2c_init(void);
+void i2c_init();
+void i2c_recover();
 uint8_t i2c_start(uint8_t address);
 uint8_t i2c_write(uint8_t data);
-uint8_t i2c_read_ack(void);
-uint8_t i2c_read_nack(void);
+uint8_t i2c_read_ack(uint8_t * data);
+uint8_t i2c_read_nack(uint8_t * data);
 uint8_t i2c_transmit(uint8_t address, uint8_t* data, uint16_t length);
 uint8_t i2c_receive(uint8_t address, uint8_t* data, uint16_t length);
 uint8_t i2c_writeReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t length);
